fab91c609775f016b8bf903adbffecefda7a37bb
GaganNaagu <GaganSG1908@gmail.com>
2026-06-26 10:06:17 +0000

    Add pool_pre_ping to async engine to fix connection is closed InterfaceError

--- database/db/session.py
+++ database/db/session.py
@@ -7,7 +7,9 @@ engine = create_async_engine(
     echo=False,
     future=True,
     pool_size=20,
-    max_overflow=10
+    max_overflow=10,
+    pool_pre_ping=True,
+    pool_recycle=1800
 )
 
 AsyncSessionLocal = async_sessionmaker(
